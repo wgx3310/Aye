e068c438a30c32a2fb0b22520ee719be1df87128
wgx3310 <wgx3378@163.com>
2016-11-19 17:15:33 +0800

    reset app default font

--- common/src/main/java/aye/CommApp.java
+++ common/src/main/java/aye/CommApp.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import aye.content.Contexts;
 import aye.net.Https;
+import aye.util.FontUtils;
 
 /**
  * Created by reid on 2016/11/14.
@@ -16,6 +17,8 @@ public class CommApp extends Application {
         super.onCreate();
         Contexts.init(getApplicationContext());
 
+        //设置系统默认字体
+        FontUtils.resetTypefaceDefaultFont("SERIF", getApplicationContext(), "fonts/default.otf");
         Https.init();
     }
 }
--- common/src/main/java/aye/util/FontUtils.java
+++ common/src/main/java/aye/util/FontUtils.java
@@ -1,9 +1,36 @@
 package aye.util;
 
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.annotation.NonNull;
+
+import java.lang.reflect.Field;
+
 /**
  * Created by reid on 2016/11/19.
  */
 
 public class FontUtils {
+    public static void resetTypefaceDefaultFont(@NonNull String name, @NonNull Context context, @NonNull String assetPath){
+        resetTypefaceDefaultFont(name, Typeface.createFromAsset(context.getAssets(), assetPath));
+    }
 
+    public static void resetTypefaceDefaultFont(@NonNull String name, @NonNull Typeface typeface){
+        modifyTypefaceField(name, typeface);
+    }
+
+    private static void modifyTypefaceField(@NonNull String fieldName, Object value) {
+        try {
+            Field defaultField = Typeface.class.getDeclaredField(fieldName);
+            defaultField.setAccessible(true);
+            defaultField.set(null, value);
+
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
